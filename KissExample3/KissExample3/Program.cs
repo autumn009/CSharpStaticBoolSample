492919a8c8547752c1550ffd338d26a98a513d20
Akira Kawamata <autumn@piedey.co.jp>
2022-09-28 11:49:50 +0900

    add KissExample3

---
+++ KissExample3/KissExample3/Program.cs
@@ -0,0 +1,21 @@
+﻿int x = 100;
+
+// case 1
+Console.WriteLine($"{x} to {tenPercentUp(x)}");
+
+int tenPercentUp(int x)
+{
+    return x * 11 / 10;
+}
+
+// case 2
+var calc = new MyCalculation();
+Console.WriteLine($"{x} to {calc.TenPercentUp(x)}");
+
+class MyCalculation
+{
+    public int TenPercentUp(int x)
+    {
+        return x * 11 / 10;
+    }
+}
