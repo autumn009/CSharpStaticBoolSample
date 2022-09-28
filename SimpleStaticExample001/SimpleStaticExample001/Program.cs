6834f1dbb81bc3db81d56c27479958cd489d24b4
Akira Kawamata <autumn@piedey.co.jp>
2022-09-28 16:39:55 +0900

    add SimpleStaticExample001

---
+++ SimpleStaticExample001/SimpleStaticExample001/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("実行を開始します");
+
+for (int i = 0; i < 12; i++)
+{
+	Console.Write($"{new A()}, ");
+}
+Console.WriteLine($"クラスAは{A.Count}回作成されました");
+
+class A
+{
+	private static int count;
+	public static int Count => count;
+	public A()
+	{
+		count++;
+	}
+	static A()
+	{
+		Console.WriteLine("クラスAが初期化されました");
+		count = 0;
+	}
+}
