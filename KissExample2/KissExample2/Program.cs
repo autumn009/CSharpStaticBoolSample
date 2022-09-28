85586aae90dce7147dc90ff58594631a45242b6d
Akira Kawamata <autumn@piedey.co.jp>
2022-09-28 10:04:12 +0900

    add Kiss Example2

---
+++ KissExample2/KissExample2/Program.cs
@@ -0,0 +1,32 @@
+﻿// case 1
+Console.WriteLine(Case1.LuckyNumbers.GetLuckyNumber());
+
+// case 2
+Console.WriteLine(Case2.LuckyNumbers.GetNumber());
+
+// case 3
+Console.WriteLine(Case3.LuckyNumbers.Get());
+
+namespace Case1
+{
+    class LuckyNumbers
+    {
+        public static int GetLuckyNumber() => 7;
+    }
+}
+
+namespace Case2
+{
+    class LuckyNumbers
+    {
+        public static int GetNumber() => 7;
+    }
+}
+
+namespace Case3
+{
+    class LuckyNumbers
+    {
+        public static int Get() => 7;
+    }
+}
