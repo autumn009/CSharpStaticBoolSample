95895012fd19f7f600b71e13e4973bfe9fd19965
Akira Kawamata <autumn@piedey.co.jp>
2022-09-29 19:19:51 +0900

    add LocalFunc001/002

---
+++ LocalFunc001/LocalFunc001/Program.cs
@@ -0,0 +1,8 @@
+﻿recur(1);
+
+void recur(int i)
+{
+    if (i > 3) return;
+    Console.WriteLine($"Count={i}");
+    recur(i + 1);
+}
---
+++ LocalFunc002/LocalFunc002/Program.cs
@@ -0,0 +1,17 @@
+﻿add(2, 1);
+sub(2, 1);
+
+void add(int x, int y)
+{
+    report(x, y, '+', x + y);
+}
+
+void sub(int x, int y)
+{
+    report(x, y, '-', x - y);
+}
+
+void report(int x, int y, char opr, int r)
+{
+    Console.WriteLine($"{x}{opr}{y}={r}");
+}
