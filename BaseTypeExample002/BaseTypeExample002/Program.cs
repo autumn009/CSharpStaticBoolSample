5a7f77caacad55082f290e9386a57a732bf799a2
Akira Kawamata <autumn@piedey.co.jp>
2022-09-30 18:31:26 +0900

    add BaseTypeExample002

---
+++ BaseTypeExample002/BaseTypeExample002/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 1;
+Sub1();
+TheClass.Sub2(()=>x);
+
+void Sub1()
+{
+    Console.WriteLine(x);
+}
+
+static class TheClass
+{
+    public static void Sub2(Func<int> peekValue)
+    {
+        Console.WriteLine(peekValue());
+    }
+}
+
