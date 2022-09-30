059e134d5958c10b4abe2f5b9ab5705b664b779f
Akira Kawamata <autumn@piedey.co.jp>
2022-09-30 16:15:44 +0900

    add Initialize001

---
+++ Initialize001/Initialize001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(TheClass.X);
+
+static class TheClass
+{
+    public static int X;
+    public static void sub()
+    {
+        Console.WriteLine(X);
+    }
+    static TheClass()
+    {
+        sub();
+        X = 123;
+    }
+}
