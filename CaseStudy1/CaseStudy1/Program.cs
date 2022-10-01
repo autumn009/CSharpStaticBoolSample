144dee0844596255296d8deccbe214e099aeebc0
Akira Kawamata <autumn@piedey.co.jp>
2022-10-01 20:00:31 +0900

    add CaseStudy1

---
+++ CaseStudy1/CaseStudy1/Program.cs
@@ -0,0 +1,35 @@
+﻿long count = 0;
+long sum = 0;
+long min = long.MaxValue;
+long max = long.MinValue;
+string minpath = "";
+string maxpath = "";
+
+foreach (var fullpath in Directory.EnumerateFiles(args[0], "*.jpg", SearchOption.AllDirectories))
+{
+    count++;
+    long size = new FileInfo(fullpath).Length;
+    sum += size;
+    if (size < min)
+    {
+        min = size;
+        minpath = fullpath;
+    }
+    if (size > max)
+    {
+        max = size;
+        maxpath = fullpath;
+    }
+    if ((count & 4095) == 1)
+    {
+        Console.Write(count);
+        Console.Write('\r');
+    }
+}
+
+Console.WriteLine($"Total Count of files: {count}");
+Console.WriteLine($"Sum of files: {sum}");
+Console.WriteLine($"Min size: {min}");
+Console.WriteLine($"Min path: {minpath}");
+Console.WriteLine($"Max size: {max}");
+Console.WriteLine($"Max path: {maxpath}");
